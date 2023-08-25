4a91bd1f76a8a5344c1e02d56277daac4ff8dcff
Annie McElroy <134727217+Annie-McElroy@users.noreply.github.com>
2023-08-24 22:00:03 -0500

    Fixed models

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,5 @@
 const { connect, connection } = require('mongoose');
 
-connect('mongodb+srv://Yuechan8522:AlexMac12@cluster0.kmtupzk.mongodb.net/developersApplications');
+connect('mongodb+srv://Yuechan8522:AlexMac12@cluster0.kmtupzk.mongodb.net/socialNetworkDB');
 
 module.exports = connection;
